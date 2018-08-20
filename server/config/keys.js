7e321d89cdfb9c782f7bcdd0fc911965de8ccf1f
leemvale <neuromiwork@gmail.com>
2018-08-20 10:15:50 +0300

    refactor

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,4 @@
 const { DB_USER, DB_PWD, DB_NAME, DB_HOST, DB_PORT } = process.env;
-console.log(DB_USER, DB_PWD, DB_NAME, DB_HOST, DB_PORT);
 
 module.exports = {
     mongoURI:`mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
